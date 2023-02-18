658a8c5f044880d9aa3c2bb1f09f7bde31cdabca
vitorramos <ramos6382@gmail.com>
2023-02-17 23:30:40 -0300

    parte 1

---
+++ SRC/index.ts
@@ -0,0 +1,105 @@
+import express, { Request, Response } from 'express'
+import cors from 'cors'
+import { User } from './models/User'
+import { UserDatabase } from './database/UserDatabase'
+import { UserDB } from './types'
+
+const app = express()
+
+app.use(cors())
+app.use(express.json())
+
+app.listen(3003, () => {
+    console.log(`Servidor rodando na porta ${3003}`)
+})
+
+app.get("/ping", async (req: Request, res: Response) => {
+    try {
+        res.status(200).send({ message: "Pong!" })
+    } catch (error) {
+        console.log(error)
+
+        if (req.statusCode === 200) {
+            res.status(500)
+        }
+
+        if (error instanceof Error) {
+            res.send(error.message)
+        } else {
+            res.send("Erro inesperado")
+        }
+    }
+})
+
+app.post("/users", async (req: Request, res: Response) => {
+    try {
+
+        const { id, name, email, password, role } = req.body
+
+        if (typeof id !== "string") {
+            res.status(400)
+            throw new Error("'id' deve ser string")
+        }
+
+        if (typeof name !== "string") {
+            res.status(400)
+            throw new Error("'name' deve ser string")
+        }
+
+        if (typeof email !== "string") {
+            res.status(400)
+            throw new Error("'email' deve ser string")
+        }
+
+        if (typeof password !== "string") {
+            res.status(400)
+            throw new Error("'password' deve ser string")
+        }
+        if (typeof role !== "string") {
+            res.status(400)
+            throw new Error("'role' deve ser string")
+        }
+
+        const userDatabase = new UserDatabase()
+        const userDBExists = await userDatabase.findUserById(id)
+
+        if (userDBExists) {
+            res.status(400)
+            throw new Error("'id' já¡ existente");
+        }
+
+        const newUser = new User(
+            id,
+            name,
+            email,
+            password,
+            role,
+            new Date().toISOString()
+        )
+
+        const newUserDB: UserDB = {
+            id: newUser.getId(),
+            name: newUser.getName(),
+            email: newUser.getEmail(),
+            password: newUser.getPassword(),
+            role: newUser.getRole(),
+            createdAt: newUser.getCreatedAt()
+        }
+
+        userDatabase.insertUser(newUserDB)
+        res.status(201).send(newUser)
+
+    } catch (error) {
+        console.log(error)
+
+        if (req.statusCode === 200) {
+            res.status(500)
+        }
+
+        if (error instanceof Error) {
+            res.send(error.message)
+        } else {
+            res.send("Erro inesperado")
+        }
+    }
+})
